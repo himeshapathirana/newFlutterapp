6d12be82d62eaa327d1b71590ae43599b9accc4c
himeshapathirana <94542536+himeshapathirana@users.noreply.github.com>
2023-07-12 15:54:02 +0530

    Update home_page.dart
    newfile

--- home_page.dart
+++ home_page.dart
@@ -7,7 +7,7 @@ const HomePage({super.key});
 @override
 Widget build(BuildContext context){
   return  Scaffold(
-    appBar:AppBar(title: const Text("Pavan Dulakshitha"),
+    appBar:AppBar(title: const Text("Himesha Pathirana"),
     elevation: 4,
      ),
     body: const Center(
